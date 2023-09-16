8cf516cc34bba22029e707c1099c003940d0bdd8
den413 <rsbyg390@yahoo.co.jp>
2023-09-16 16:37:21 +0900

    change index.js

--- api/index.js
+++ api/index.js
@@ -6,11 +6,11 @@ const PORT = 5050;
 require("dotenv").config();
 const cors = require("cors");
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//   })
+// );
 app.use(express.json());
 app.use("/api/v1", require("../src/v1/routes"));
 
